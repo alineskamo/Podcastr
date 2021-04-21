90c7548f0e7300ede711bcda8e80752ba4b78c30
alineskamo <alineskamo@gmail.com>
2021-04-21 19:23:52 -0300

    Finalização da segunda aula

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+//SPA -> single page apliccation
+//SSR -> server side render
+//SSG -> static side generation
+
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  )
+}
+
+// Será executado todas vez que alguém acessar o site
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+
+    revalidate: 60 * 60 * 8, //tempo que a página será atualizada 
+  }
+
+}; 
